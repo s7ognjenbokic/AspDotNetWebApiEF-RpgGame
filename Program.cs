d4b43c87648850887ff7b54d46e731772a695f63
s7ognjenbokic <ognjen.bokic@s7codedesign.com>
2023-03-08 09:42:31 +0100

    Database configuration

--- Program.cs
+++ Program.cs
@@ -2,17 +2,22 @@ global using udemy_dotnet_webapi.Models;
 global using udemy_dotnet_webapi.Services.CharacterService;
 global using udemy_dotnet_webapi.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using udemy_dotnet_webapi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
