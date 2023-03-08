e391bf41c3576125054227bf0d0fafecf0983ebd
s7ognjenbokic <ognjen.bokic@s7codedesign.com>
2023-03-08 13:12:59 +0100

    Username column renamed

--- Models/User.cs
+++ Models/User.cs
@@ -8,7 +8,7 @@ namespace udemy_dotnet_webapi.Models
     public class User
     {
         public int Id { get; set; }
-        public string UserName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[0];
         public byte[] PasswordSalt { get; set; } = new byte[0];
         public List<Character>? Characters { get; set; }
